ad3fa31ecf562369f5e378dd6a87c12de2ea19d1
cgnangui <coffi-desire.gnanguy@etudiant.univ-rennes.fr>
2025-02-15 01:40:00 +0100

    projet flask maj 1

--- app/config.py
+++ app/config.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'default_cle_secrete')
-    SQLALCHEMY_DATABASE_URI =os.getenv("DATABASE_URL", "postgresql://flask_db_jgkv_user:9YyoKlzJNcdLJ82I9OcUpuqPhif0M9cl@dpg-cuntmudsvqrc739905v0-a.oregon-postgres.render.com/flask_db_jgkv")    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI =os.getenv("DATABASE_URL", "postgresql://flask_db_jgkv_user:9YyoKlzJNcdLJ82I9OcUpuqPhif0M9cl@dpg-cuntmudsvqrc739905v0-a.oregon-postgres.render.com/flask_db_jgkv")    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads', 'profile_pics')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
